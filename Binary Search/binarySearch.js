bec3b2a89c78cd1e4b448d02d524e31cb4de975a
fring <banedictfring12@gmail.com>
2023-04-25 15:26:08 +0600

    added binary search

--- Binary Search/binarySearch.js
+++ Binary Search/binarySearch.js
@@ -1,6 +1,19 @@
 var search = function (nums, target) {
-  
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const guess = nums[mid];
+
+    guess === target
+      ? (high = mid - 1)
+      : guess > target
+      ? (high = mid - 1)
+      : (low = mid + 1);
+  }
+
+  return nums[low] === target ? low : -1;
 };
 
-
-console.log("result", search([-1,0,3,5,9,12],9));
+console.log("result", search([-1, 0, 3, 5, 9, 12], 9));
