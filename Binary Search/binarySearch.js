86070287389dfcb0dc6832d6ac2e9de1094776ae
fring <banedictfring12@gmail.com>
2023-04-25 13:45:20 +0600

    binary search added

---
+++ Binary Search/binarySearch.js
@@ -0,0 +1,6 @@
+var search = function (nums, target) {
+  
+};
+
+
+console.log("result", search([-1,0,3,5,9,12],9));
---
+++ Stack/stackBasicOperations.js
@@ -0,0 +1,12 @@
+class Stack {
+    constructor{
+      this.items = []
+      this.count = 0
+    }
+}
+
+const stack = new Stack();
+
+stack.push(100)
+stack.push(200)
+stack.push(300)
